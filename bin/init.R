32005ee1c99af7c95c506fa685e5b35267748bae
Elise Courtemanche <elise@boonlogic.com>
2022-05-11 12:19:11 -0500

    debug

--- bin/init.R
+++ bin/init.R
@@ -1,6 +1,9 @@
 if (!("renv" %in% installed.packages())) {
 	install.packages("renv", repos = "https://repo.miserver.it.umich.edu/cran/")
 }
+
+print(getwd())
+
 renv::init()
 
 packages <- list("httr",
