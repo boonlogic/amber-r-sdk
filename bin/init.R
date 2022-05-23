3ce1346664097a6b6f4417a3010e75be819cc685
Elise Courtemanche <elise@boonlogic.com>
2022-05-23 16:03:41 -0500

    remove suggests from package install

--- bin/init.R
+++ bin/init.R
@@ -2,26 +2,24 @@ if (!("renv" %in% installed.packages())) {
 	install.packages("renv", repos = "https://repo.miserver.it.umich.edu/cran/")
 }
 
-renv::settings$package.dependency.fields(c("Imports", "Depends", "LinkingTo", "Suggests"))
 renv::init()
 
-# packages <- list("testthat",
-# 				 "devtools",
-# 				 "stringr")
-# 				 # "httr",
-# 		 		#  "jsonlite",
-# 		 		#  "R6",
-# 		 		#  "fs",
-# 		 		#  "rlang",
-# 		 		#  "iterators",
-# 		 		#  "botor",
-# 		 		#  "base64enc")
+packages <- list("devtools",
+				 "stringr")
+				 # "httr",
+		 		#  "jsonlite",
+		 		#  "R6",
+		 		#  "fs",
+		 		#  "rlang",
+		 		#  "iterators",
+		 		#  "botor",
+		 		#  "base64enc")
 
-# for (package in packages) {
-# 	if (!(package %in% installed.packages())) {
-# 		# install.packages(package, repos = "https://repo.miserver.it.umich.edu/cran/")
-# 		renv::install(package)
-# 	}
-# }
+for (package in packages) {
+	if (!(package %in% installed.packages())) {
+		# install.packages(package, repos = "https://repo.miserver.it.umich.edu/cran/")
+		renv::install(package)
+	}
+}
 
 # renv::snapshot()
